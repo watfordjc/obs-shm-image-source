9ac2b68858e33875ac93af37754b6e4e66895301
John Cook <coder-a599-7927@johncook.co.uk>
2020-09-10 05:40:55 +0100

    Fill in copyright notice

--- src/plugin-main.c
+++ src/plugin-main.c
@@ -1,20 +1,4 @@
-/*
-Plugin Name
-Copyright (C) <Year> <Developer> <Email Address>
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along
-with this program. If not, see <https://www.gnu.org/licenses/>
-*/
+/* Copyright notice and (GPL-2.0-or-later) licence notice at end of file */
 
 #include <obs-module.h>
 
@@ -33,3 +17,22 @@ void obs_module_unload()
 {
     blog(LOG_INFO, "plugin unloaded");
 }
+
+
+/*
+obs-shm-image-source
+Copyright (C) 2020 John Cook <https://JohnCook.UK>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program. If not, see <https://www.gnu.org/licenses/>
+*/
